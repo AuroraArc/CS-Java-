65db78bd47adec32e014f30942cc4eb72edc8c4a
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2022-11-13 23:30:30 -0600

    06 book problems

--- 06/Book Problems/Checkerboard.java
+++ 06/Book Problems/Checkerboard.java
@@ -28,4 +28,15 @@ public class Checkerboard extends JComponent {
             g2.translate(-80, 10);
         }
     }
+    public static void main(String [] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Checkerboard box = new Checkerboard();
+        frame.add(box);
+
+        frame.setVisible(true);
+    }
 }
+
